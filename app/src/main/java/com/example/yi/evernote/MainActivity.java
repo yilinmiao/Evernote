aa0a05f5db9c0c6d053228d27b9afe7fae04327c
yilinmiao <yi.matheson@gmail.com>
2016-09-11 23:52:24 -0400

    added CursorLoader

--- app/src/main/java/com/example/yi/evernote/MainActivity.java
+++ app/src/main/java/com/example/yi/evernote/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.yi.evernote;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
+    private CursorAdapter cursorAdapter;
+
+    //    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
         InsertNote("New note");
 
 //        Cursor cursor = getContentResolver().query(NotesProvider.CONTENT_URI,DBOpenHelper.ALL_COLUMNS, null, null, null, null);
+        String[] from = {DBOpenHelper.NOTE_TEXT};
+        int[] to = {android.R.id.text1};
+        cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, null, from, to, 0);
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
+
+        getLoaderManager().initLoader(0, null, this);
+
     }
 
     private void InsertNote(String noteText) {
@@ -24,4 +42,20 @@ public class MainActivity extends AppCompatActivity {
         Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI, values);
         Log.d("MainActivity", "Inserted note " + noteUri.getLastPathSegment());
     }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this, NotesProvider.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        cursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
+    }
+
 }
